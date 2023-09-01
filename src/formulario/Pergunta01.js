4b2431ccf1aebfbcccd60ed1d682bcf439452822
Rafael-rms <marquesrafael3@hotmail.com>
2023-09-01 20:57:45 -0300

    Atualização da tela de formulario

--- src/formulario/Pergunta01.js
+++ src/formulario/Pergunta01.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'rea
 
 export default Pergunta01 = ({navigation}) => {
     return(
-        <ScrollView>
+         <ScrollView>
             <View style={styles.container}>
                 {/* Cabeçalho da tela formulário */}
                 <View style={styles.cabecalho}>
@@ -22,14 +22,16 @@ export default Pergunta01 = ({navigation}) => {
                 <>
                     <Text style={[styles.textUsuario, {fontSize:22}]}>Nome do usuário</Text>
                     <Text style={styles.textUsuario}>00/00/0000</Text>
-                    <Text style={[styles.textUsuario, {marginLeft:290}]}>01/19</Text>
+                    <Text style={[styles.textUsuario, {marginLeft:'70%'}]}>01/19</Text>
                 </>
+
                 {/* caixa de perguntas do formulário */}
                 <View style={styles.caixaPergunta}>
                     <Text style={styles.textoCaixaPergunta}> A senhora/você tem filhos(as) com o(a) agressor(a)? (Caso não tenham filhos em comum, o registro não se aplica)
                     Em caso afirmativo, estão vivendo algum conflito com relação àguarda dos filhos, visitas ou pagamento de pensão pelo agressor?</Text>
                 </View>
-                <View>
+
+                <View style={styles.containerBotoes}> 
                     {/* Botões do formulário */}
                     <TouchableOpacity 
                     onPress={()=>{}}
@@ -63,7 +65,7 @@ export default Pergunta01 = ({navigation}) => {
                     
                 </View>
             </View>
-        </ScrollView>
+         </ScrollView>
     )
 }
 
@@ -72,6 +74,9 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor: '#FFDFDF',
         alignItems:'center',
+    },
+    containerBotoes:{
+        marginBottom:'20%'
     },
     cabecalho:{
         flexDirection: 'row',
