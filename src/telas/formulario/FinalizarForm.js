d0c30ebffc116bb9ac3dfe0813053dc09c180e05
Rafael-rms <marquesrafael3@hotmail.com>
2023-10-28 00:19:17 -0300

    Atualização da tela de finalizar formulário

--- src/telas/formulario/FinalizarForm.js
+++ src/telas/formulario/FinalizarForm.js
@@ -1,19 +1,76 @@
 import react from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Cabecalho from "../../componentes/Cabecalho";
+import Linha from "../../componentes/Linha";
 
-export default function() {
-    return(
+export default function ({navigation}) {
+    return (
         <View style={Estilos.container}>
-            <Text>Rafael</Text>
+
+            <Cabecalho
+                texto="Formulário"
+                imagemEsquerda={require('../../assets/voltar.png')}
+                imagemDireita={require('../../assets/home.png')}
+                botaoEsquerda={{
+                    onPress: () => navigation.goBack(),
+                }} 
+                botaoDireita={{
+                    onPress: () => navigation.navigate('Home'),
+                }}/>
+
+            <Linha />
+
+            <View style={Estilos.containerCard}>
+                <Text style={Estilos.textosTitulos}>Nome da usuaria</Text>
+                <Text style={Estilos.textosDados}>Nome da usuaria</Text>
+                <Text style={Estilos.textosTitulos}>Data de Nascimento</Text>
+                <Text style={Estilos.textosDados}>00/00/0000</Text>
+                <Text style={Estilos.textosTitulos}>Data da realização</Text>
+                <Text style={Estilos.textosDados}>00/00/0000</Text>
+
+                <TouchableOpacity style={Estilos.botaoExibirForm}>
+                    <Text style={Estilos.textoExibirForm}>Exibir Formulário</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const Estilos = StyleSheet.create({
-    container:{
-        backgroundColor:'green',
-        flex:1,
+    container: {
+        backgroundColor: '#FFDFDF',
+        flex: 1,
+        alignItems: 'center',
+        // justifyContent: 'center'
+    },
+    containerCard: {
+        backgroundColor: '#EDCECE',
+        width: '90%',
+        height: '60%',
+        borderRadius:20,
         alignItems:'center',
-        justifyContent:'center'
+        justifyContent:'center',
+        marginTop:60
+    },
+    textosTitulos: {
+        fontSize: 30,
+        fontWeight:'bold',
+        margin:10
+    },
+    textosDados: {
+        fontSize: 25
+    },
+    botaoExibirForm:{
+        backgroundColor:'#D69595',
+        height:40,
+        width:'65%',
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:'10%',
+        borderRadius:20
+    },
+    textoExibirForm:{
+        fontSize:25,
+        fontWeight:'bold'
     }
 })
